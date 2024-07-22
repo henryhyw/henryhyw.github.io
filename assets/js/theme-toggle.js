90fabf8511c90caadf851e63f3bc8b63a8aad2bf
henry-wang117 <henry.why@connect.hku.hk>
2024-07-22 18:41:09 +0800

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleThemeBtn = document.getElementById('toggle-theme');
 
+    // Function to update the color of all elements with the class 'typed' based on the theme
+    function updateTypedElementsColor() {
+        const typedElements = document.querySelectorAll('.typed');
+        const isDarkMode = document.body.classList.contains('dark-mode');
+        const textColor = isDarkMode ? '#fafafa' : '#252525';
+
+        typedElements.forEach(element => {
+            element.style.color = textColor;
+        });
+
+        console.log(`Updated color of ${typedElements.length} elements to ${textColor}`);
+    }
+
     // Function to apply the theme
     function applyTheme(theme) {
         document.body.classList.toggle('dark-mode', theme === 'dark');
+        updateTypedElementsColor();
     }
 
     // Load the theme from localStorage
