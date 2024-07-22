4acb518e9a79cfbe5b407b8895d7df3e3c0ae8f9
henry-wang117 <henry.why@connect.hku.hk>
2024-07-22 17:44:14 +0800

    Update theme-toggle.js

--- assets/css/theme-toggle.js
+++ assets/css/theme-toggle.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleThemeBtn = document.getElementById('toggle-theme');
 
+    // Function to update the color of the subtitle based on the theme
+    function updateSubtitleColor() {
+        const subtitleElement = document.getElementById('welcomeSubtitle');
+        const isDarkMode = document.body.classList.contains('dark-mode');
+        console.log(isDarkMode);
+        const textColor = isDarkMode ? '#fafafa' : '#252525';
+        subtitleElement.style.color = textColor;
+        console.log(textColor);
+    }
+
     // Function to apply the theme
     function applyTheme(theme) {
         document.body.classList.toggle('dark-mode', theme === 'dark');
@@ -10,16 +20,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Load the theme from localStorage
     const savedTheme = localStorage.getItem('theme') || 'light';
     applyTheme(savedTheme);
-
-    // Function to update the color of the subtitle based on the theme
-    function updateSubtitleColor() {
-        const subtitleElement = document.getElementById('welcomeSubtitle');
-        const isDarkMode = document.body.classList.contains('dark-mode');
-        console.log("isdarkMode: " + isDarkMode);
-        const textColor = isDarkMode ? '#fafafa' : '#252525';
-        subtitleElement.style.color = textColor;
-        console.log("subtitle change to " + textColor);
-    }
 
     // Toggle theme on button click
     toggleThemeBtn.addEventListener('click', () => {
