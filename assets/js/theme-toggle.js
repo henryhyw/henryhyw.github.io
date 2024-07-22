35212635d96811f62586ea54f9acef17083a4310
henry-wang117 <henry.why@connect.hku.hk>
2024-07-22 18:54:33 +0800

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -14,20 +14,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.log(`Updated color of ${typedElements.length} elements to ${textColor}`);
     }
 
-    // Function to update the script src based on the theme
-    function updateScriptSrc() {
+    // Function to update the map script based on the theme
+    function updateMapScript() {
+        const existingScript = document.getElementById('mapmyvisitors');
+        if (existingScript) {
+            existingScript.remove();
+        }
+
+        const newScript = document.createElement('script');
+        newScript.type = 'text/javascript';
+        newScript.id = 'mapmyvisitors';
+        
         const isDarkMode = document.body.classList.contains('dark-mode');
-        const lightSrc = 'https://mapmyvisitors.com/map.js?cl=606060&w=a&t=n&d=NuzI5fMF9fqCHtkxcTx3LZO5mvAbEZrLLxG3ZW1E-KY&co=ffffff&ct=606060';
-        const darkSrc = 'https://mapmyvisitors.com/map.js?cl=fafafa&w=a&t=n&d=NuzI5fMF9fqCHtkxcTx3LZO5mvAbEZrLLxG3ZW1E-KY&co=000000&cmo=3acc3a&cmn=ff5353&ct=808080';
-        mapScript.src = isDarkMode ? darkSrc : lightSrc;
-        console.log(`Updated script src to ${mapScript.src}`);
+        if (isDarkMode) {
+            newScript.src = 'https://mapmyvisitors.com/map.js?cl=fafafa&w=a&t=n&d=NuzI5fMF9fqCHtkxcTx3LZO5mvAbEZrLLxG3ZW1E-KY&co=000000&cmo=3acc3a&cmn=ff5353&ct=808080';
+        } else {
+            newScript.src = 'https://mapmyvisitors.com/map.js?cl=606060&w=a&t=n&d=NuzI5fMF9fqCHtkxcTx3LZO5mvAbEZrLLxG3ZW1E-KY&co=ffffff&ct=606060';
+        }
+
+        document.body.appendChild(newScript);
     }
 
     // Function to apply the theme
     function applyTheme(theme) {
         document.body.classList.toggle('dark-mode', theme === 'dark');
         updateTypedElementsColor();
-        updateScriptSrc();
+        updateMapScript();
     }
 
     // Load the theme from localStorage
