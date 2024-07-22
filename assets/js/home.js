96f5454b1b8cbca6d59f0b64f325586c084c431f
henry-wang117 <henry.why@connect.hku.hk>
2024-07-23 02:58:46 +0800

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -119,8 +119,6 @@ function checkVideoCompatibility() {
     videoElement.addEventListener('error', () => {
         videoElement.style.display = 'none';
         fallbackImage.style.display = 'block';
-        fallbackImage.style.transition = 'opacity 2s ease-in-out';
-        fallbackImage.style.opacity = '';
         adjustTitle(); // Ensure text formatting is adjusted when fallback image is shown
         const isSmallScreen = window.matchMedia("(max-width: 600px)").matches;
         if (isSmallScreen) {
