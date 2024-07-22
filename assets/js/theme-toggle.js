ef1d7b907cc488647bb3d317451a10e635a13100
henry-wang117 <henry.why@connect.hku.hk>
2024-07-22 18:19:11 +0800

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -4,16 +4,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Function to apply the theme
     function applyTheme(theme) {
         document.body.classList.toggle('dark-mode', theme === 'dark');
-        const subtitleText = document.getElementById('welcomeSubtitle').textContent;
-        const subtitleElement = document.getElementById('welcomeSubtitle');
-        subtitleElement.style.textAlign = 'left'; // Initially set to left align
-        subtitleElement.style.textAlignLast = 'left'; // Initially set to left align
-        subtitleElement.style.MozTextAlignLast = 'left'; // Initially set to left align
-        typeWriterEffect(subtitleText, subtitleElement, 15, () => {
-            subtitleElement.style.textAlign = 'justify'; // Change to justify after typing is complete
-            subtitleElement.style.textAlignLast = 'justify'; // Change to justify after typing is complete
-            subtitleElement.style.MozTextAlignLast = 'justify'; // Change to justify after typing is complete
-        });
     }
 
     // Load the theme from localStorage
@@ -26,5 +16,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const newTheme = currentTheme === 'light' ? 'dark' : 'light';
         applyTheme(newTheme);
         localStorage.setItem('theme', newTheme); // Save the new theme in localStorage
+        const subtitleText = document.getElementById('welcomeSubtitle').textContent;
+        const subtitleElement = document.getElementById('welcomeSubtitle');
+        subtitleElement.style.textAlign = 'left'; // Initially set to left align
+        subtitleElement.style.textAlignLast = 'left'; // Initially set to left align
+        subtitleElement.style.MozTextAlignLast = 'left'; // Initially set to left align
+        typeWriterEffect(subtitleText, subtitleElement, 15, () => {
+            subtitleElement.style.textAlign = 'justify'; // Change to justify after typing is complete
+            subtitleElement.style.textAlignLast = 'justify'; // Change to justify after typing is complete
+            subtitleElement.style.MozTextAlignLast = 'justify'; // Change to justify after typing is complete
+        });
     });
 });
