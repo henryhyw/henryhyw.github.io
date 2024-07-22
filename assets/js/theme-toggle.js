0669d4095f0fe33db7f21fb6d9501df5ec645075
henry-wang117 <henry.why@connect.hku.hk>
2024-07-22 19:05:59 +0800

    1

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -14,32 +14,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.log(`Updated color of ${typedElements.length} elements to ${textColor}`);
     }
 
-    // Function to update the map script based on the theme
-    function updateMapScript(theme) {
-        const existingScript = document.getElementById('mapmyvisitors');
-        if (existingScript) {
-            existingScript.remove();
-        }
+    // Function to toggle map visibility based on the theme
+    function toggleMapVisibility() {
+        const lightMap = document.querySelector('.lightmap');
+        const darkMap = document.querySelector('.darkmap');
+        const isDarkMode = document.body.classList.contains('dark-mode');
 
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.id = 'mapmyvisitors';
-
-        if (theme === 'dark') {
-            script.src = 'https://mapmyvisitors.com/map.js?cl=fafafa&w=a&t=n&d=NuzI5fMF9fqCHtkxcTx3LZO5mvAbEZrLLxG3ZW1E-KY&co=000000&cmo=3acc3a&cmn=ff5353&ct=808080';
+        if (isDarkMode) {
+            lightMap.style.display = 'none';
+            darkMap.style.display = 'block';
         } else {
-            script.src = 'https://mapmyvisitors.com/map.js?cl=606060&w=a&t=n&d=NuzI5fMF9fqCHtkxcTx3LZO5mvAbEZrLLxG3ZW1E-KY&co=ffffff&ct=606060';
+            lightMap.style.display = 'block';
+            darkMap.style.display = 'none';
         }
-
-        // Ensure the script runs by appending to the document head
-        document.head.appendChild(script);
     }
 
     // Function to apply the theme
     function applyTheme(theme) {
         document.body.classList.toggle('dark-mode', theme === 'dark');
         updateTypedElementsColor();
-        updateMapScript();
+        toggleMapVisibility();
     }
 
     // Load the theme from localStorage
