988b4a93ce1e946bb6fc2556abaaa9dd8450eda7
henry-wang117 <henry.why@connect.hku.hk>
2024-07-22 17:23:35 +0800

    Update theme-toggle.js

--- assets/css/theme-toggle.js
+++ assets/css/theme-toggle.js
@@ -4,11 +4,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Function to apply the theme
     function applyTheme(theme) {
         document.body.classList.toggle('dark-mode', theme === 'dark');
+        updateSubtitleColor();
     }
 
     // Load the theme from localStorage
     const savedTheme = localStorage.getItem('theme') || 'light';
     applyTheme(savedTheme);
+
+    // Function to update the color of the subtitle based on the theme
+    function updateSubtitleColor() {
+        const subtitleElement = document.getElementById('welcomeSubtitle');
+        const isDarkMode = document.body.classList.contains('dark-mode');
+        const textColor = isDarkMode ? '#fafafa' : '#252525';
+        subtitleElement.style.color = textColor;
+    }
 
     // Toggle theme on button click
     toggleThemeBtn.addEventListener('click', () => {
