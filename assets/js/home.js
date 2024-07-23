15d99177bd044e40d9f02d99a18a55d6f05d2f74
henry-wang117 <henry.why@connect.hku.hk>
2024-07-24 01:29:51 +0800

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -185,22 +185,22 @@ function checkVideoCompatibility() {
     const fallbackImage = document.getElementById('imageElement');
     videoElement.style.transition = 'opacity 2s ease-in-out';
     videoElement.style.opacity = '1';
-
-    // Attempt to play the video, if it fails, switch to the fallback image
-    videoElement.play().catch(() => {
-        videoElement.style.display = 'none';
-        fallbackImage.style.opacity = '0';
-        fallbackImage.style.display = 'block';
-        fallbackImage.style.opacity = 'opacity 4s ease-in-out';
-        fallbackImage.style.opacity = '';
-        adjustTitle(); // Ensure text formatting is adjusted when fallback image is shown
-        const isSmallScreen = window.matchMedia("(max-width: 600px)").matches;
-        if (isSmallScreen) {
-            document.getElementById('welcomeTitle').style.fontSize = '2em';
-            document.getElementById('compassIcon').style.fontSize = '1.1em';
-        }
-        adjustSubtitle();
-    });
+    videoElement.play();
+    // // Attempt to play the video, if it fails, switch to the fallback image
+    // videoElement.play().catch(() => {
+    //     videoElement.style.display = 'none';
+    //     fallbackImage.style.opacity = '0';
+    //     fallbackImage.style.display = 'block';
+    //     fallbackImage.style.opacity = 'opacity 4s ease-in-out';
+    //     fallbackImage.style.opacity = '';
+    //     adjustTitle(); // Ensure text formatting is adjusted when fallback image is shown
+    //     const isSmallScreen = window.matchMedia("(max-width: 600px)").matches;
+    //     if (isSmallScreen) {
+    //         document.getElementById('welcomeTitle').style.fontSize = '2em';
+    //         document.getElementById('compassIcon').style.fontSize = '1.1em';
+    //     }
+    //     adjustSubtitle();
+    // });
 }
 
 const videoSources = [
