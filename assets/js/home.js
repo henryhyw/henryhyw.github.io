62b9c5f770515784957ddca883d23c20bca0ce16
henry-wang117 <henry.why@connect.hku.hk>
2024-07-23 22:42:58 +0800

    1

--- assets/js/home.js
+++ assets/js/home.js
@@ -166,7 +166,7 @@ document.getElementById('compassIcon').addEventListener('click', function() {
     
     // Calculate new angle and duration based on the number of presses
     let newAngle = Math.min(15 + pressCount * 6, maxAngle); // Increase angle by 5 degrees per press, up to maxAngle
-    let newDuration = Math.max(0.8 - pressCount * 0.05, maxDuration); // Decrease duration by 0.05s per press, down to maxDuration
+    let newDuration = Math.max(0.8 - pressCount * 0, maxDuration); // Decrease duration by 0.05s per press, down to maxDuration
     
     shakeQueue.push({ newAngle, newDuration });
     processShakeQueue();
