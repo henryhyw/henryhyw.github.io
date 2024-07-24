3d7a187388c2378b4bbc59ebe8cbb3e3c7db262c
henry-wang117 <henry.why@connect.hku.hk>
2024-07-25 04:22:59 +0800

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -25,6 +25,7 @@ function changeVideoSource() {
     const videoElement = document.getElementById('videoElement');
     const currentSourceElement = videoElement.querySelector('source');
     currentSourceElement.setAttribute('src', source.src)
+    currentSourceElement.classList.add(source.class);
     videoElement.load();
 }
 
