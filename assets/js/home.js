e1299cf9e9919bfa00a6b513de3522c0f75fc38e
henry-wang117 <henry.why@connect.hku.hk>
2024-07-25 03:18:57 +0800

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -324,7 +324,7 @@ function createFlashingHint() {
         compassIcon.classList.add("tipcolor-1");
         setTimeout(function() {
             compassIcon.classList.remove("tipcolor-1");
-        }, 2000);
+        }, 1500);
     }
 
     // Set an interval to toggle the classes every 4000ms if pressed is false
@@ -334,7 +334,7 @@ function createFlashingHint() {
         } else {
             clearInterval(hintFlash); // Clear the interval
         }
-    }, 4000);
+    }, 3000);
 }
 
 function displayWelcomeContent() {
