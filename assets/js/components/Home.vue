7c6b2686448bcbe144e6e06941ba062a01fffd51
henry-wang117 <henry.why@connect.hku.hk>
2024-07-27 08:07:53 +0800

    1

--- assets/js/components/Home.vue
+++ assets/js/components/Home.vue
@@ -2,10 +2,7 @@
   <div v-html="compiledMarkdown"></div>
 </template>
 
-<script type="module">
-import axios from 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
-import MarkdownIt from 'https://cdn.jsdelivr.net/npm/markdown-it/dist/markdown-it.min.js';
-
+<script>
 export default {
   data() {
     return {
@@ -14,12 +11,12 @@ export default {
   },
   computed: {
     compiledMarkdown() {
-      const md = new MarkdownIt();
+      const md = window.markdownit();
       return md.render(this.markdownContent);
     },
   },
   mounted() {
-    axios
+    window.axios
       .get('/assets/markdown/index.md') // Update the path to your Markdown file
       .then((response) => {
         this.markdownContent = response.data;
